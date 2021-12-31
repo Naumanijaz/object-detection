972269bead2c5e8ca1571c46c00e33bdbc3e8bb3
Naumanijaz <74481334+Naumanijaz@users.noreply.github.com>
2021-12-30 21:33:06 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+var img=""
+objects=[]
+var status=""
+
+
+function draw(){
+image(video, 0, 0, 380, 380);
+if(status!=""){
+    r=random(255);
+    g=random(255);
+    b=random(255);
+objectDetector.detect(video, gotResult)
+for(i=0;i<objects.length;i++)
+{
+    document.getElementById("status").innerHTML = "Status : Objects Detected";
+    document.getElementById("ObJect").innerHTML = "Number Of Objects :"+objects.length;
+    fill(r,g,b);
+    percent=floor(objects[i].confidence*100)  
+    
+    text(objects[i].label+" "+percent+"%", objects[i].x, objects[i].y) 
+    noFill();
+    stroke(r,g,b)
+    rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+}}}
+
+function setup(){
+canvas = createCanvas(380, 380);
+canvas.center();
+video=createCapture(VIDEO)
+video.hide()
+objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+function modelLoaded(){
+    console.log("Model Loaded!")
+    status = true;
+    
+}
+function preload(){
+
+}
+function gotResult(error, results){
+    if (error){
+        console.log(error);
+    }
+    console.log(results)
+    objects=results
+}
